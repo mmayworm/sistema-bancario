f7ae5b2e21a4118ce58debd6b6d90cee5e97b21d
Marcos <mmayworm1979@gmail.com>
2025-08-15 23:42:56 -0300

    implementando todo o menu e todas as funções

--- banking_system.py
+++ banking_system.py
@@ -1,7 +1,10 @@
+import os
+
 # Módulo depositar
 def depositar(valor):
-    if valor>0:        
-        return valor
+    if valor>0:
+        extrato = f'Depósito {valor}\n'        
+        return valor, extrato
     else: 
         print("Valor deve ser maior que 0")
 
@@ -10,49 +13,68 @@ def sacar(valor):
     if(valor<1):
         print('valor deve ser maior que zero')   
     elif(saldo<valor):
-        print(f'Saldo insuficiente!{saldo}')        
+        print(f'Saldo insuficiente! {saldo}')        
     elif(valor>500):
         print('Limite de saque é de R$500.00')
-      
-    else: 
-        return valor          
+    elif(quantidade_saque>=2):    
+        print('Limite de saque é de 3x por dia')
+    else:
+        extrato = f'Saque: {valor}\n'  
+        return valor, extrato          
 
 limite_saque = 0
 saldo = 0
+extrato = ''
+quantidade_saque = 0
 
 # menu de opções
 
 opcao_menu = ''
-while (opcao_menu!=4):
-    print('''#### Menu ####
-          #### opção 1: depositar####
-          #### opção 2: sacar ####
-          #### opção 4: sair ####
-          ''')
-    opcao_menu = int(input('Digite a opção desejada: '))
-    #opção de depósito
-    if(opcao_menu ==1):
-        valor = float(input('depósito: '))
-        operacao = depositar(valor)    
 
-        #atualização do saldo
+while (opcao_menu!='4'):
+    print('''
+        MENU:
+        [1] - Depositar
+        [2] - Sacar
+        [3] - Extrato
+        [4] - Sair  
+    ''')
+    opcao_menu = input('Digite a opção desejada: ')
+    #opção de depósito
+    if(opcao_menu =='1'):
+        valor = float(input('Valor para Depósito: '))
+        operacao = depositar(valor)        
+
+        #atualização do saldo e extrato
         if(operacao!=None):
-            saldo+= operacao
-            print(f'Valor depositado: {valor}')
-            print(f'saldo final = {saldo}')
+            saldo+= operacao[0]
+            print(f'Valor depositado: {valor}\n')
+            extrato = extrato + operacao[1]
+            
 
     #opção de saque
-    elif(opcao_menu==2):
-        valor = float(input('sacar: '))
-        operacao = sacar(valor)
+    elif(opcao_menu=='2'):
+        valor = float(input('Valor do Saque: '))
+        if(quantidade_saque>=2):
+            print('Quantidade de saque diário ultrapassado')
+        else:    
+            operacao = sacar(valor)
 
         #atualização do saldo
-        if(operacao!=None):
-            saldo-= operacao
+        if(operacao!=None and quantidade_saque<=2):
+            saldo-= operacao[0]
+            quantidade_saque+=1
             print(f'Valor sacado: {valor}')
             print(f'saldo final = {saldo}')
+            extrato = extrato + operacao[1]
 
-    elif(opcao_menu==4):
+    #opção de extrato
+    elif(opcao_menu=='3'):
+        print(f'{extrato}Saldo do dia: {saldo}')        
+            
+    elif(opcao_menu=='4'):
         print('Fim das operações!')
+        input('Pressione uma tecla para sair...')
+        os.system('cls')
 
     else: print('opção inválida')
